094a5c4089160abf2b4fcf73eadc3195803a1bfb
Benjamin Cisneros Barraza <cisnerosbarraza@gmail.com>
2023-12-05 15:44:16 -0600

    Apply exam 2 code

---
+++ app/src/main/java/mx/tecnm/delicias/poo/unit2/exam/Book.java
@@ -0,0 +1,83 @@
+package mx.tecnm.delicias.poo.unit2.exam;
+
+/**
+ * Book class.
+ *
+ * @author Benjamin Cisneros Barraza
+ */
+public class Book {
+  private static int bookCount;
+  private final String title;
+  private final String author;
+  private final int pages;
+  private double price = 100;
+  private boolean available = true;
+
+  /**
+   * Constructor of Book.
+   *
+   * @param title The book title.
+   * @param author The book author.
+   * @param pages The number of pages of the book.
+   */
+  public Book(final String title, final String author, final int pages) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+
+    bookCount += 1;
+  }
+
+  @Override
+  public String toString() {
+    return "Book [title="
+        + title
+        + ", author="
+        + author
+        + ", pages="
+        + pages
+        + ", price="
+        + price
+        + ", available="
+        + available
+        + "]";
+  }
+
+  public void setPrice(final double newPrice) {
+    this.price = newPrice;
+  }
+
+  public void setAvailable(final boolean isAvailable) {
+    this.available = isAvailable;
+  }
+
+  public double getPrice() {
+    return this.getPrice(0);
+  }
+
+  /**
+   * Gets the book price with a given discount, limited to 15%.
+   *
+   * @param discount Represents the % of discount.
+   */
+  public double getPrice(final int discount) {
+    final var allowedDiscount = Math.min(discount, 15);
+    final var priceDiscounted = (this.price * allowedDiscount) / 100;
+    return this.price - priceDiscounted;
+  }
+
+  /**
+   * Returns the number of books created.
+   *
+   * @return an integer that represents the number of books created.
+   */
+  public static int getBookCount() {
+    return bookCount;
+  }
+
+  /** Prints the book information. */
+  @SuppressWarnings("PMD.SystemPrintln")
+  public void print() {
+    System.out.println(this);
+  }
+}
---
+++ app/src/main/java/mx/tecnm/delicias/poo/unit2/exam/Exam.java
@@ -0,0 +1,41 @@
+package mx.tecnm.delicias.poo.unit2.exam;
+
+/**
+ * Unit 2 Exam class.
+ *
+ * @author Benjamin Cisneros Barraza.
+ */
+public class Exam {
+  /**
+   * Main method to test Exam.
+   *
+   * @param args The main args.
+   */
+  public static void main(final String[] args) {
+    new Exam().doExam();
+  }
+
+  @SuppressWarnings("PMD.SystemPrintln")
+  private void doExam() {
+    final var quijoteBook = new Book("Don Quijote de la Mancha", "Miguel Cervantes", 3000);
+    quijoteBook.print();
+    printBookCount();
+
+    System.out.println("Regular price: " + quijoteBook.getPrice());
+    System.out.println("Price with 10% discount: " + quijoteBook.getPrice(10));
+
+    final var harryPotterBook = new Book("Harry Potter", "JK Rowling", 1500);
+    harryPotterBook.setPrice(200);
+    harryPotterBook.setAvailable(false);
+    harryPotterBook.print();
+    printBookCount();
+
+    System.out.println("Regular price: " + harryPotterBook.getPrice());
+    System.out.println("Price with 15% discount: " + harryPotterBook.getPrice(15));
+  }
+
+  @SuppressWarnings("PMD.SystemPrintln")
+  private void printBookCount() {
+    System.out.println("Book count: " + Book.getBookCount());
+  }
+}
