ab69bbd78790443b98590810feaa8dc62bd11351
Benjamin Cisneros Barraza <cisnerosbarraza@gmail.com>
2023-12-06 02:18:25 -0600

    Add BookTest

---
+++ app/src/test/java/mx/tecnm/delicias/poo/unit2/exam/BookTest.java
@@ -0,0 +1,50 @@
+package mx.tecnm.delicias.poo.unit2.exam;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class BookTest {
+
+  @Test
+  void shouldGetPriceWithoutDiscount() {
+    var book = createAnyBook();
+    var price = 300.0d;
+    book.setPrice(price);
+
+    assertEquals(price, book.getPrice(), "Should calculate price without discount");
+  }
+
+  @Test
+  void shouldGetPriceWithDiscount() {
+    var book = createAnyBook();
+    var price = 1000.0d;
+    book.setPrice(price);
+
+    assertEquals(900d, book.getPrice(10), "Should calculate price with discount");
+  }
+
+  @Test
+  @SuppressWarnings("PMD.JUnitTestContainsTooManyAsserts")
+  void canCountNumberOfBooksCreated() {
+    assertEquals(0, Book.getBookCount(), "Book count should be 0");
+    createAnyBook();
+    assertEquals(1, Book.getBookCount(), "Book count should be 1");
+    createAnyBook();
+    assertEquals(2, Book.getBookCount(), "Book count should be 2");
+  }
+
+  @Test
+  void canGetItsDataAsString() {
+    var book = createAnyBook();
+
+    assertEquals(
+        "Book [title=Any book, author=Any author, pages=100, price=100.0, available=true]",
+        book.toString(),
+        "Should get Book String representation");
+  }
+
+  private Book createAnyBook() {
+    return new Book("Any book", "Any author", 100);
+  }
+}
