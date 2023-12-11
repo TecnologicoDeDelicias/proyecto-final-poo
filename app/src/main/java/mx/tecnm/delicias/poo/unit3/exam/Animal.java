9a0f0e84b8b5fd189d1e9f72004a79c1fa00201f
Antonio Vazquez Reyes <toniu211@gmail.com>
2023-12-10 20:23:39 -0600

    test

---
+++ app/src/main/java/mx/tecnm/delicias/poo/unit3/exam/Animal.java
@@ -0,0 +1,14 @@
+package mx.tecnm.delicias.poo.unit3.exam;
+
+public class Animal {
+
+  public Animal(String string) {}
+
+  public String getSpecie() {
+    return "Unknown";
+  }
+
+  public String greeting() {
+    return "Hello, my name is Boris the Animal and I am of Unknown specie.";
+  }
+}
---
+++ app/src/main/java/mx/tecnm/delicias/poo/unit3/exam/Dog.java
@@ -0,0 +1,19 @@
+package mx.tecnm.delicias.poo.unit3.exam;
+
+public class Dog extends Animal {
+
+  private String fav;
+
+public Dog(String name, String fav) {
+    super (name);
+    this.fav= fav;
+  }
+
+  public String getSpecie() {
+    return null;
+  }
+
+  public String greeting() {
+    return null;
+  }
+}
