a467019d3e8ad06729e7ee5909ba57b6f3be1fb0
Victor <victorarredondo431@gmail.com>
2023-12-11 17:25:17 -0700

    Passing test

--- app/src/main/java/mx/tecnm/delicias/poo/unit5/exam/Calculator.java
+++ app/src/main/java/mx/tecnm/delicias/poo/unit5/exam/Calculator.java
@@ -2,19 +2,31 @@ package mx.tecnm.delicias.poo.unit5.exam;
 
 public class Calculator {
 
-  public int unsafeDivision(int i, int j) {
-    return (2);
+  public int unsafeDivision(int first, int second) {
+    return first / second;
   }
 
-  public int safeDivision(int i, int j) {
-    return i/j;
+  public int safeDivision(int first, int second) {
+    try {
+      return unsafeDivision (first,second);
+    } catch (ArithmeticException ae){
+     return 0;
+    }  
   }
 
-  public int uncheckedDivision(int i, int j) {
-    return i/j;
+  public int uncheckedDivision(int first, int second) {
+    try {
+      return unsafeDivision (first,second);
+    } catch (ArithmeticException ae){
+      throw new ArithmeticException ("Cannot divide by zero");
+    }  
   }
 
-  public int checkedDivision(int i, int j) {
-    return i/j;
-  }
+  public int checkedDivision(int first, int second)throws DivisionByZero {
+    try {
+      return unsafeDivision (first,second);
+    } catch (ArithmeticException ae){
+      throw new DivisionByZero (ae);
+    }  
+}
 }
